5941ce9e9fb8c1fc3eb351ba33247b5d66d674d8
Leos Julien <osx.leos@gmail.com>
2020-10-16 12:35:12 +0200

    Update doc

--- algorithm.py
+++ algorithm.py
@@ -13,18 +13,24 @@ class Algorithm():
         self.config = config
 
     def runNextInstance(self, newCandle, allCandles):
-        # Example algorithm which open a buy position if there is no opened position and close it otherwise
+        # Example algorithm which:
+        # - Open a buy position if there is no opened position
+        # - Try to close it imediatly but failed most of the time (see below why)
+        # - Close the position if at least one in opened
+
         if len(self.fxcm.getPositions()) == 0:
             self.positionId = self.fxcm.buy(1)
             print("Buy position %s" % self.positionId)
 
-            # Try to close the position right after opening it (might not work each time because FXCM API needs 1-2 seconds to transform an order into a real open position)
+            # Try to close the position right after opening it (might not work each time because FXCM API needs between 0 and 2000 ms to transform an order into a real open position)
             self.closePosition(self.positionId)
         else:
             self.closePosition(self.positionId)
 
     def closePosition(self, positionId):
+        # Always check closePosition return to know if the close was succesfull or not
         if self.fxcm.closePosition(positionId):
             print("Close position %s" % positionId)
         else:
             print("Position %s couldn't be closed" % positionId)
+
